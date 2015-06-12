f0d9943e58f11654dc93c38f9db651c1498648b9
royriojas <roy@kno.com>
2015-06-12 00:34:26 -0700

    BLD: Release v1.0.0

--- specs/index.js
+++ specs/index.js
@@ -1,18 +1,3 @@
-[![NPM Version](http://img.shields.io/npm/v/is-null-like.svg?style=flat)](https://npmjs.org/package/is-null-like)
-[![Build Status](http://img.shields.io/travis/royriojas/is-null-like.svg?style=flat)](https://travis-ci.org/royriojas/is-null-like)
-
-# is-null-like
-A simple javascript function that will return true if the value is null like (aka undefined or null)
-
-## Installation
-
-```bash
-npm i --save is-null-like
-```
-
-## Overview
-
-```javascript
 describe( 'is-null-like', function () {
   it( 'should return true in case of null', function () {
     var isNullLike = require( '../' );
@@ -33,9 +18,3 @@ describe( 'is-null-like', function () {
     expect( isNullLike( {} ) ).to.be.false;
   } );
 } );
-```
-
-## Changelog
-
-[changelog](./changelog.md)
-
